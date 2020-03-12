1663dd0d25c5b54284e2360e47facc45f7dadfdc
KarenOn <55865352+KarenOn@users.noreply.github.com>
2020-03-11 21:04:34 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prototype_shallow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal am = new Animal() { Nombre = "Perro Mauricio", Patas = 4 };
+            Animal amclon = am.Clone() as Animal;
+            amclon.Patas = 5;
+
+            Console.WriteLine(am.Patas);
+            Console.ReadKey();
+        }
+    }
+}
